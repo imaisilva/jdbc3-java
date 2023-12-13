9016e1f5e0d5b303d8ecbe96e91c69c235ba35c1
Mayara Silva <alohasp@outlook.com>
2023-12-13 20:11:19 -0300

    Retrieving id

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,7 +2,9 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
@@ -19,11 +21,13 @@ public class Program {
 		try {
 			conn = DB.getConnection();
 			
+			//EXEMPLO 1:
 			st = conn.prepareStatement(
 					"INSERT INTO seller"
 					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
 					+ "VALUES"
-					+"(?, ?, ?, ?, ?)");
+					+"(?, ?, ?, ?, ?)",
+					Statement.RETURN_GENERATED_KEYS);
 			
 			st.setString(1, "Carl Purple");
 			st.setString(2, "carl@gmail.com");
@@ -31,9 +35,25 @@ public class Program {
 			st.setDouble(4, 3000.0);
 			st.setInt(5, 4);
 			
+			
+			/* EXEMPLO 2:
+			st = conn.prepareStatement(
+			"insert into department (Name) values ('D1'), ('D2')",
+					Statement.RETURN_GENERATED_KEYS);
+			*/
+			
 			int rowsAffected = st.executeUpdate();
 			
-			System.out.println("Done! Rows affected: " + rowsAffected);
+			if(rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				while (rs.next()) {
+					int id = rs.getInt(1);
+					System.out.println("Done! Id: " + id);
+				}
+			}
+			else {
+				System.out.println("No rows affected!");
+			}
 			
 		}
 		catch (SQLException e) {
